b380555a7095e2f2b66a6dd39c06060c84f1fc80
magicwithoutu <msasaki.a@gmail.com>
2014-10-17 13:01:15 -0500

    add bootstrap template

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__) # initialize Flask with name of the module
 manager = Manager(app) # initialize CLI manager
+bootstrap = Bootstrap(app) # initialize bootstrap templates
 
 @app.route('/')
 def index():
